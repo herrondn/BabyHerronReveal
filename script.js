ee0a2aeadf4a1496226e2d807e5aa68b841659af
herrondn <herrondn@gmail.com>
2025-12-22 22:21:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 const GENDER = "girl"; // "boy" or "girl"
 
 // Set your reveal date/time (local time)
-// const revealDate = new Date("2025-12-29T21:00:00").getTime();
-const revealDate = new Date("2025-12-22T22:15:00").getTime();
+const revealDate = new Date("2025-12-29T21:00:00").getTime();
+// const revealDate = new Date("2025-12-22T22:15:00").getTime();
 
 const startButton = document.getElementById("start-button");
 const startScreen = document.getElementById("start-screen");
